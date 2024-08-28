17682519e667e7a403d196575c4f17e7cf1d7bde
AAA <theakhgar@gmail.com>
2024-08-28 14:40:24 +0330

    loading into redis

--- index.mjs
+++ index.mjs
@@ -16,22 +16,83 @@ import './sec.mjs'
 import './rng.mjs'
 import  './eckey.mjs'
 import Base58 from 'base-58'
+import { createClient } from 'redis';
+import readline from 'readline'
+import fs from 'fs'
+import process from 'process'
+
+
+/**
+ * Load Redis
+ */
+var client = null
+try {
+    client = createClient({
+        url: 'redis://default:1234@192.168.0.144:6379'
+    })
+    
+    await client.connect();
+    console.log('[START] Connected to Redis');
+} catch (e) {
+    console.error(e)
+}
+
+//Load the file into redis
+const LOAD  = true
+if (LOAD==true){
+    try{
+        
+        await client.flushDb()
+        const readInterface = readline.createInterface({
+            input: fs.createReadStream('btah.txt'),
+            console: false
+        });
+        var i = 1;
+        var wallets = []
+        readInterface.on('line', function(line) {
+            console.log(i);
+            if(!line.startsWith("1") || i>22685755){
+                readInterface.close()
+                return;
+            }
+            console.clear()
+            wallets.push(line)
+            i++;
+        });
+
+        //start by loading into redis
+        readInterface.on('close',async()=>{
+            for (var i=0;i<wallets.length;i++){
+                await client.set(wallets[i],"1")
+                console.clear()
+                console.log("Loading > ",i)
+            }
+            console.log("Load Finished.")
+            process.exit(0)
+        })
+    }catch(e){
+        console.log(e)
+    }
+
+
+   // process.exit(0)
+
+}
 
 
 
 var i =0;
-while (i<1){
+while (!LOAD &&  i<1){
     var btc = new Bitcoin.ECKey();
     console.log("Private Key > ",btc.getExportedPrivateKey())
-    console.log("BTC Addr> ",btc.getBitcoinAddress().toString())
-
     console.log("Private Key Hex> ",btc.toString())
+
+    console.log("BTC Addr> ",btc.getBitcoinAddress().toString())
     console.log("Pub Addr> ",Base58.encode(btc.getPub()))
     
     i++;
 }
 
-//console.log(new Bitcoin.ECKey().getExportedPrivateKey())
 
 
 
